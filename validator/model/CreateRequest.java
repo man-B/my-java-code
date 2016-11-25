a7411806b16202c590a5f4857194c0ac840c390e
man-B <bopardikar.mangesh@gmail.com>
2016-11-25 11:19:49 +1100

    Create CreateRequest.java

---
+++ validator/model/CreateRequest.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import validator.ValidCreateRequest;
+
+@ValidCreateRequest
+public class CreateClaimRequest implements Serializable {
+
+}
